9da133f7bd2ad97b15973293e78acbbeb4afd9db
satyam07 <satyam905@gmail.com>
2015-11-19 15:04:41 +0530

    Tweaks

--- dash/views.py
+++ dash/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'dash/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
